47fc23564a03c6d019b0ef7ab4e674bd39ede85d
MaxAMillionL <maxwelllloyd@comcast.net>
2025-03-21 00:07:27 -0400

    Defensive copies on all functions???? Maybe???

--- symtablelist.c
+++ symtablelist.c
@@ -104,6 +104,7 @@ int SymTable_put(SymTable_T oSymTable, const char *pcKey,
     strLength = strlen(pcKey) + 1;
     pKey = (char*) malloc(strLength * sizeof(char));
     if(pKey == NULL){
+        free(pNewNode);
         return 0;
     }
     strcpy(pKey, pcKey);
@@ -123,13 +124,22 @@ void *SymTable_replace(SymTable_T oSymTable, const char *pcKey,
     struct SymTableNode *pCurrentNode;
     const void* pOldValue;
     int found = 0;
+    char *pKey;
+    size_t strLength;
 
     assert(oSymTable != NULL);
     assert(pcKey != NULL);
 
+    strLength = strlen(pcKey) + 1;
+    pKey = (char*) malloc(strLength * sizeof(char));
+    if(pKey == NULL){
+        return 0;
+    }
+    strcpy(pKey, pcKey);
+
     pCurrentNode = oSymTable->pFirstNode;
     while(pCurrentNode != NULL && !found){
-        if(pCurrentNode->pKey == pcKey){
+        if(pCurrentNode->pKey == pKey){
             found = 1;
             pOldValue = pCurrentNode->pValue;
             pCurrentNode->pValue = pvValue;
@@ -144,9 +154,18 @@ void *SymTable_replace(SymTable_T oSymTable, const char *pcKey,
 
 int SymTable_contains(SymTable_T oSymTable, const char *pcKey){
     struct SymTableNode *pCurrentNode;
+    char *pKey;
+    size_t strLength;
 
     assert(oSymTable != NULL);
     assert(pcKey != NULL);
+
+    strLength = strlen(pcKey) + 1;
+    pKey = (char*) malloc(strLength * sizeof(char));
+    if(pKey == NULL){
+        return 0;
+    }
+    strcpy(pKey, pcKey);
 
     pCurrentNode = oSymTable->pFirstNode;
     while(pCurrentNode != NULL){
